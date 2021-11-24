ac5b9eafc1e11baf8d66a4a098b982ec0e6ac8d2
JulianeGampe <juliane.gampe@gmail.com>
2021-11-24 08:52:06 +0000

    Create the function to start the game

--- run.py
+++ run.py
@@ -1,13 +1,30 @@
 import random
 
+points = 50
+
 def start_game():
     """
     Starts a new game with an initial 50 points
     Displays game instructions
     """
     print("Welcome to the Greedy Gremlin!")
+    print("How to play:")
+    print("To win the game: Collect 500 points!")
+    print("You will play several rounds to achieve that.")
+    print("You will start with an initial number of 50 points.")
+    print("You will be given a choice between three envelopes: red, green, blue.")
+    print("One of them contains a positive number of points.")
+    print("One contains a negative number of points.")
+    print("One contains the Greedy Gremlin, who will take all your points for himself.")
+    print("After choosing an envelope the content of one of the others will be revealed.")
+    print("You can continue to open your envelope or skip the round\nat the cost of 50 points.")
+    print("The game is lost if you open the envelope with the Greedy Gremlin\nor if your score turns negative.")
+    print("-------------------------------------------")
+    print(f"You have {points} points.")
+    print("-------------------------------------------")
+    start_new_round(points)
 
-def start_new_round():
+def start_new_round(points):
     """
     Starts a new round of choosing an envelope
     """
@@ -52,4 +69,6 @@ def main():
     """
     Runs all program functions
     """
-    pass
+    start_game()
+
+main()
