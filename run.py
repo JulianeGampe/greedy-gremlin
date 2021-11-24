c6004e973af5a9de5dd5a57432c26274f175aa41
JulianeGampe <juliane.gampe@gmail.com>
2021-11-24 08:21:41 +0000

    Set up the basic function structure

--- run.py
+++ run.py
@@ -1,4 +1,55 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
-print("Welcome to the Greedy Gremlin!")
+import random
+
+def start_game():
+    """
+    Starts a new game with an initial 50 points
+    Displays game instructions
+    """
+    print("Welcome to the Greedy Gremlin!")
+
+def start_new_round():
+    """
+    Starts a new round of choosing an envelope
+    """
+    pass
+
+class Envelope:
+    """
+    Creates the envelopes
+    Sets color, content
+    Has a method to open the envelope and to skip the envelope
+    """
+    pass
+
+def calculate_points():
+    """
+    Calculates the number of points after each round
+    """
+    pass
+
+def play_new_round():
+    """
+    Allows the player that has a positive number of points to:
+    Start a new round or exit the game
+    """
+    pass
+
+def play_again():
+    """
+    Displays after a win or game over
+    Allows the player to choose to start a new game or exit
+    """
+    pass
+
+def exit():
+    """
+    Exits the game completely
+    Displays Goodbye message
+    """
+    pass
+
+def main():
+    """
+    Runs all program functions
+    """
+    pass
